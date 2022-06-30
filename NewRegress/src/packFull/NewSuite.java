13a4f31031dd770c65aa1cf37f2fabc0b14f940c
pravinkpawase <pravinpawase98@gmail.com>
2022-06-30 09:13:50 +0530

    First Test
    First Test Execution Completed please Review it

---
+++ NewRegress/src/packFull/NewSuite.java
@@ -0,0 +1,42 @@
+package packFull;
+
+import org.testng.annotations.Test;
+
+public class NewSuite {
+@Test
+public void test1() {
+	System.out.println("Test1 is Running");
+}
+@Test
+public void test2() {
+	System.out.println("Test2 is Running");
+}
+@Test
+public void test3() {
+	System.out.println("Test3 is Running");
+}
+@Test
+public void test4() {
+	System.out.println("Test4 is Running");
+}
+@Test
+public void test5() {
+	System.out.println("Test5 is Running");
+}
+@Test
+public void test6() {
+	System.out.println("Test6 is Running");
+}
+@Test
+public void test7() {
+	System.out.println("Test7 is Running");
+}
+@Test
+public void test8() {
+	System.out.println("Test8 is Running");
+}
+@Test
+public void test9() {
+	System.out.println("Test9 is Running");
+}
+}
