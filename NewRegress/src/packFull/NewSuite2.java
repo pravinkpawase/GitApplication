5b921e35d24d8ad2df3b5556fa41fe9079d2c5a8
pravinkpawase <pravinpawase98@gmail.com>
2022-06-30 09:18:55 +0530

    NewFeature
    Feature Completed plese merge it to master branch

---
+++ NewRegress/src/packFull/NewSuite2.java
@@ -0,0 +1,42 @@
+package packFull;
+
+import org.testng.annotations.Test;
+
+public class NewSuite2 {
+@Test
+public void test51() {
+	System.out.println("Test51 is Running");
+}
+@Test
+public void test52() {
+	System.out.println("Test52 is Running");
+}
+@Test
+public void test3() {
+	System.out.println("Test53 is Running");
+}
+@Test
+public void test4() {
+	System.out.println("Test54 is Running");
+}
+@Test
+public void test5() {
+	System.out.println("Test55 is Running");
+}
+@Test
+public void test6() {
+	System.out.println("Test56 is Running");
+}
+@Test
+public void test7() {
+	System.out.println("Test57 is Running");
+}
+@Test
+public void test8() {
+	System.out.println("Test58 is Running");
+}
+@Test
+public void test9() {
+	System.out.println("Test59 is Running");
+}
+}
